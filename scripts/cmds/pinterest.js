43547c353bbe9f618418d914765047de9fd2d357
XAIKO <ayanchowdhury55505@gmail.com>
2024-05-27 21:27:03 +0600

    Update pinterest.js

--- scripts/cmds/pinterest.js
+++ scripts/cmds/pinterest.js
@@ -1,57 +1,65 @@
 const axios = require("axios");
 const fs = require("fs-extra");
-const request = require("request");
 const path = require("path");
-
+const request = require("request");
+/*Do not change
+        the credit 🙂👍🏻*/
+ 
 module.exports = {
   config: {
-    name: "pinterest",
-    aliases: ["pin", " pic"],
-    version: "1.0.2",
-    author: "ᴍʀ.ᴀʏᴀɴ",
+    name: "pic",
+    aliases: ["pinterest", "pin", "photo"],
+    version: "1.0",
+    author: "𝐌𝐑.𝐀𝐘𝐀𝐍",
     role: 0,
-    countDown: 5,
-    shortDescription: {
-      en: "Search for images on Pinterest"
-    },
+    countDown: 20,
     longDescription: {
-      en: ""
+      en: "Get Image From Pinterest",
     },
     category: "Search",
     guide: {
-      en: "{prefix}pinterest <search query> -<number of images>"
+      en: "{pn} <search query> <number of images>\nExample: {pn} Hacker -10"
+    },
+    langs: {
+      "en": {
+          "missing": '{pn} anime girl - 10'
+      }
     }
   },
 
-  onStart: async function ({ api, event, args }) {
+  onStart: async function ({ api, event, args, message }) {
     try {
-      const keySearch = args.join(" ");
-      if (!keySearch.includes("-")) {
-        return api.sendMessage(`ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛʜᴇ sᴇᴀʀᴄʜ ǫᴜᴇʀʏ ᴀɴᴅ ɴᴜᴍʙᴇʀ ᴏғ ɪᴍᴀɢᴇs ᴛᴏ ʀᴇᴛᴜʀɴ ɪɴᴛʜᴇ ғᴏʀᴍᴀᴛ: ${config.guide.en}`, event.threadID, event.messageID);
-      }
-      const keySearchs = keySearch.substr(0, keySearch.indexOf('-')).trim();
-      const numberSearch = Math.max(parseInt(keySearch.split("-").pop().trim()) || 5, 5);
-
-      const res = await axios.get(`https://api-dien.kira1011.repl.co/pinterest?search=${encodeURIComponent(keySearchs)}`);
-      const data = res.data.data;
-      const imgData = [];
-
-      for (let i = 0; i < Math.min(numberSearch, data.length); i++) {
-        const imgResponse = await axios.get(data[i], { responseType: 'arraybuffer' });
-        const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
-        await fs.outputFile(imgPath, imgResponse.data);
-        imgData.push(fs.createReadStream(imgPath));
-      }
-
-      await api.sendMessage({
-        attachment: imgData,
-        body: `Here are the top ${imgData.length} image results for "${keySearchs}":`
-      }, event.threadID, event.messageID);
-
-      await fs.remove(path.join(__dirname, 'cache'));
-    } catch (error) {
-      console.error(error);
-      return api.sendMessage(`ᴘʟᴇᴀsᴇ ᴀᴅᴅ ᴛᴏ ʏᴏᴜʀ ᴋᴇʏsᴇᴀʀᴄʜ -5 \n ᴇ.ɢ., ᴘɪɴ -ᴄᴀᴛ -5`, event.threadID, event.messageID);
+    const keySearch = args.join(" ");
+  const { spotify, pintarest } = require('nayan-server')
+    if(keySearch.includes("-") == false) 
+      return message.reply("𝗣𝗹𝗲𝗮𝘀𝗲 𝗲𝗻𝘁𝗲𝗿 𝘁𝗵𝗲 𝘀𝗲𝗮𝗿𝗰𝗵 𝗾𝘂𝗲𝗿𝘆 𝗮𝗻𝗱 - 𝗻𝘂𝗺𝗯𝗲𝗿 𝗼𝗳 𝗶𝗺𝗮𝗴𝗲𝘀 (1-50)", event.threadID, event.messageID)
+    const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
+    const numberSearch = keySearch.split("-").pop() || 6
+    const res = await pintarest(`${encodeURIComponent(keySearchs)}`);
+  console.log(res)
+    const data = res.data;
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
     }
-  }
-};
+    message.reply({
+        attachment: imgData,
+        body: numberSearch + ' images for '+ keySearchs
+    }, event.threadID, event.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    };
+      } catch (error) {
+      console.error(error);
+      return api.sendMessage(
+        `An error occurred.`,
+        event.threadID,
+        event.messageID
+      );
+}
+}
+      }
